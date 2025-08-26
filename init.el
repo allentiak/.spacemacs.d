0e54fef2a7d5f5a73ce1acfc0de15bd6377c92f4
Leandro Doctors <ldoctors@gmail.com>
2025-08-26 12:25:12 +0200

    Update font size

--- init.el
+++ init.el
@@ -354,11 +354,14 @@ It should only modify the values of Spacemacs settings."
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
    dotspacemacs-default-font '("Go Mono"
-                               ;; for 1.5K screen
+                               ;; for 1K screen
                                ;; :size 12.0
 
+                               ;; for 1.5K screen
+                               :size 16.0
+
                                ;; for 4K screen
-                               :size 20.0
+                               ;; :size 20.0
                                :weight normal
                                :width normal)
 
